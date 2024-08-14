ee47918c9c9ef97608f2895160ff1caa08b771de
Faris Rabie <farisrabie@Fariss-MacBook-Pro.local>
2024-08-14 14:50:59 +0300

    shift private varibles to env and initialize firebase db and storage

--- src/lib/firebase.config.js
+++ src/lib/firebase.config.js
@@ -1,26 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyCe6xTcXcQJCjblPkxl3W43EoFKH2k_hWA",
-	authDomain: "image-hoard.firebaseapp.com",
-	projectId: "image-hoard",
-	storageBucket: "image-hoard.appspot.com",
-	messagingSenderId: "746779420567",
-	appId: "1:746779420567:web:66efe00115382fafd386d2",
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
 };
+console.log("firebaseConfig: ", firebaseConfig);
 
 // Initialize Firebase
-const app = () => {
-	if (!firebaseConfig || !firebaseConfig.apiKey) {
-		throw new Error(`No firebase config object provided ...`);
-	} else {
-		console.log("Firebase Inialized ...");
-	}
-	return initializeApp(firebaseConfig);
-};
-
-export default app
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export default app;
