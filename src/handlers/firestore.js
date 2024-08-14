4fb9f3af83ef181177e278a12b219ea76012d6a9
Faris Rabie <farisrabie@Fariss-MacBook-Pro.local>
2024-08-14 15:46:46 +0300

    implement db models for read/write of DB

---
+++ src/handlers/firestore.js
@@ -0,0 +1,43 @@
+import { collection, doc, getDocs, serverTimestamp, setDoc } from "firebase/firestore";
+import { db } from "../lib/firebase.config";
+
+const fireStore = {
+	readDocs(...args) {
+		let docs = [];
+		const ref = collection(db, "stocks");
+		return new Promise(async (resolve, reject) => {
+			try {
+				const snapshots = await getDocs(ref);
+				snapshots.forEach((doc) => {
+					const d = { ...doc.data() };
+					docs.push(d);
+				});
+				resolve(docs);
+			} catch (e) {
+				console.error("Error read document: ", e);
+				reject(new Error("woops"));
+			}
+		});
+	},
+	writeDoc(...args) {
+		const [inputs, collection_name] = args;
+		return new Promise(async (resolve, reject) => {
+			const randomIndex = Math.floor(Math.random() * 100000000);
+			try {
+				const docRef = doc(db, "stocks", `${randomIndex}`);
+				await setDoc(docRef, {
+					title: inputs.title,
+					path: inputs.path,
+					createdAt: serverTimestamp(),
+				});
+				console.log("Document successfully written!");
+				resolve("new doc successfully inserted");
+			} catch (e) {
+				console.error("Error writing document: ", e);
+				reject(new Error("woops"));
+			}
+		});
+	},
+};
+
+export default fireStore;
